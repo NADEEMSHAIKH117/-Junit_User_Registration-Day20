d933874ead876865f03419eaf8c69681d352ee0d
NADEEMSHAIKH117 <nadeemshaikh1171998@gmail.com>
2021-11-18 03:14:05 +0530

    added

--- src/test/java/com/bridgeLabz/TestEmailIdsTest.java
+++ src/test/java/com/bridgeLabz/TestEmailIdsTest.java
@@ -1,130 +1,58 @@
 package com.bridgeLabz;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+    @RunWith(Parameterized.class)
 
-public class TestEmailIdsTest {
-	TestEmailIds testEmailIds = new TestEmailIds();
+    public class TestEmailIdsTest {
+        private String emailIds;
+        private boolean expectedResult;
 
-    @Test
-    public void givenEmailId1_IsProper_ReturnTrue() {
-        boolean actualResult = testEmailIds.testForValidEmails("abc@yahoo.com");
-        assertEquals(true, actualResult);
-    }
+        public TestEmailIdsTest(String emailIds, boolean expectedResult) {
+            this.emailIds = emailIds;
+            this.expectedResult = expectedResult;
+        }
 
-    @Test
-    public void givenEmailId2_IsProper_ReturnTrue() {
-        boolean actualResult = testEmailIds.testForValidEmails("abc-100@yahoo.com");
-        assertEquals(true, actualResult);
-    }
+        @Parameterized.Parameters
+        public static Collection mailIdsExpectedResult() {
+            return Arrays.asList(new Object[][]{
+                    {"abc@yahoo.com", true},
+                    {"abc-100@yahoo.com", true},
+                    {"abc.100@yahoo.com", true},
+                    {"abc111@abc.com", true},
+                    {"abc-100@abc.net", true},
+                    {"abc.100@abc.com.au", true},
+                    {"abc@1.com", true},
+                    {"abc@gmail.com.com", true},
+                    {"abc+100@gmail.com", true},
+                    {"abc", false},
+                    {"abc@.com.my", false},
+                    {"abc123@gmail.a", false},
+                    {"abc123@.com", false},
+                    {"abc123@.com.com", false},
+                    {".abc@abc.com", false},
+                    {"abc()*@gmail.com", false},
+                    {"abc@%*.com", false},
+                    {"abc..2002@gmail.com", false},
+                    {"abc.@gmail.com", false},
+                    {"abc@abc@gmail.com", false},
+                    {"abc@gmail.com.1a", false},
+                    {"abc@gmail.com.aa.au", false},
+            });
+        }
 
-    @Test
-    public void givenEmailId3_IsProper_ReturnTrue() {
-        boolean actualResult = testEmailIds.testForValidEmails("abc.100@yahoo.com");
-        assertEquals(true, actualResult);
+        @Test
+        public void givenEmailIds_withExpectedResult_shouldPassAllTheTestCases() {
+            TestEmailIds testEmailIds = new TestEmailIds();
+            boolean result = testEmailIds.emailIdValidator(this.emailIds);
+            Assert.assertEquals(this.expectedResult, result);
+        }
     }
-
-    @Test
-    public void givenEmailId4_IsProper_ReturnTrue() {
-        boolean actualResult = testEmailIds.testForValidEmails("abc111@abc.com");
-        assertEquals(true, actualResult);
-    }
-
-    @Test
-    public void givenEmailId5_IsProper_ReturnTrue() {
-        boolean actualResult = testEmailIds.testForValidEmails("abc-100@abc.net");
-        assertEquals(true, actualResult);
-    }
-
-    @Test
-    public void givenEmailId6_IsProper_ReturnTrue() {
-        boolean actualResult = testEmailIds.testForValidEmails("abc.100@abc.com.au");
-        assertEquals(true, actualResult);
-    }
-
-    @Test
-    public void givenEmailId7_IsProper_ReturnTrue() {
-        boolean actualResult = testEmailIds.testForValidEmails("abc@1.com");
-        assertEquals(true, actualResult);
-    }
-
-    @Test
-    public void givenEmailId8_IsProper_ReturnTrue() {
-        boolean actualResult = testEmailIds.testForValidEmails("abc@gmail.com.com");
-        assertEquals(true, actualResult);
-    }
-
-    @Test
-    public void givenEmailId9_IsProper_ReturnTrue() {
-        boolean actualResult = testEmailIds.testForValidEmails("abc+100@gmail.com");
-        assertEquals(true, actualResult);
-    }
-
-    @Test
-    public void givenEmilId1_IsNotProper_ReturnFalse() {
-        boolean actualResult = testEmailIds.testForNonValidEmails("abc");
-        assertEquals(false, actualResult);
-    }
-    @Test
-    public void givenEmilId2_IsNotProper_ReturnFalse() {
-        boolean actualResult = testEmailIds.testForNonValidEmails("abc@.com.my");
-        assertEquals(false, actualResult);
-    }
-    @Test
-    public void givenEmilId3_IsNotProper_ReturnFalse() {
-        boolean actualResult = testEmailIds.testForNonValidEmails("abc123@gmail.a");
-        assertEquals(false, actualResult);
-    }
-    @Test
-    public void givenEmilId4_IsNotProper_ReturnFalse() {
-        boolean actualResult = testEmailIds.testForNonValidEmails("abc123@.com");
-        assertEquals(false, actualResult);
-    }
-    @Test
-    public void givenEmilId5_IsNotProper_ReturnFalse() {
-        boolean actualResult = testEmailIds.testForNonValidEmails("abc123@.com.com");
-        assertEquals(false, actualResult);
-    }
-    @Test
-    public void givenEmilIds6_IsNotProper_ReturnFalse() {
-        boolean actualResult = testEmailIds.testForNonValidEmails(".abc@abc.com");
-        assertEquals(false, actualResult);
-    }
-    @Test
-    public void givenEmilId7_IsNotProper_ReturnFalse() {
-        boolean actualResult = testEmailIds.testForNonValidEmails("abc()*@gmail.com");
-        assertEquals(false, actualResult);
-    }
-    @Test
-    public void givenEmilId8_IsNotProper_ReturnFalse() {
-        boolean actualResult = testEmailIds.testForNonValidEmails("abc@%*.com");
-        assertEquals(false, actualResult);
-    }
-    @Test
-    public void givenEmilId9_IsNotProper_ReturnFalse() {
-        boolean actualResult = testEmailIds.testForNonValidEmails("abc..2002@gmail.com");
-        assertEquals(false, actualResult);
-    }
-    @Test
-    public void givenEmilId10_IsNotProper_ReturnFalse() {
-        boolean actualResult = testEmailIds.testForNonValidEmails("abc.@gmail.com");
-        assertEquals(false, actualResult);
-    }
-    @Test
-    public void givenEmilId11_IsNotProper_ReturnFalse() {
-        boolean actualResult = testEmailIds.testForNonValidEmails("abc@abc@gmail.com");
-        assertEquals(false, actualResult);
-    }
-    @Test
-    public void givenEmilId12_IsNotProper_ReturnFalse() {
-        boolean actualResult = testEmailIds.testForNonValidEmails("abc@gmail.com.1a");
-        assertEquals(false, actualResult);
-    }
-    @Test
-    public void givenEmilId13_IsNotProper_ReturnFalse() {
-        boolean actualResult = testEmailIds.testForNonValidEmails("abc@gmail.com.aa.au");
-        assertEquals(false, actualResult);
-    }
-	
-}
